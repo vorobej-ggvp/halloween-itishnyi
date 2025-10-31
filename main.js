fde3da6eed532e7a2b2c43c76359544e709f95f9
winimak <oleg.nikolaiciuk@gmail.com>
2025-10-31 20:45:33 +0200

    visibiliti update

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 let mainButt = document.getElementById("mainButt");
+let textClick = document.getElementById("numOfClicks");
+let firstUpdate = document.getElementById("firstUpdate");
 
 let numOfClicks = 0;
+
 mainButt.onclick = function() {
     numOfClicks++;
+    textClick.textContent = "Button clicked " + numOfClicks + " times";
+    
+    if (numOfClicks === 30) {
+        console.log("X");
+        firstUpdate.style.visibility = "visible";
+    }
 }
 
+firstUpdate.onclick = function() {
+    if (numOfClicks >= 30) {
+        document.body.style.backgroundImage = "url('img/background.jpg')";
+    }}
