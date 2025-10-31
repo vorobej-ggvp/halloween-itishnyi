3c778ba2e7515f16c4de3de954ba5ae33fad7527
winimak <oleg.nikolaiciuk@gmail.com>
2025-11-01 00:01:39 +0200

    style update

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
 let mainButt = document.getElementById("mainButt");
 let textClick = document.getElementById("numOfClicks");
 let firstUpdate = document.getElementById("firstUpdate");
+let secondUpdate = document.getElementById("secondUpdate");
 
 let numOfClicks = 0;
+let firstStage = false;
+let secondStage = false;
 
 mainButt.onclick = function() {
     numOfClicks++;
     textClick.textContent = "Button clicked " + numOfClicks + " times";
     
     if (numOfClicks === 30) {
-        console.log("X");
         firstUpdate.style.visibility = "visible";
+    }else if (numOfClicks === 50) {
+        secondUpdate.style.visibility = "visible";
     }
+
+    if (firstStage === true) {
+    numOfClicks++;
+    }else if(secondStage === true) {
+    numOfClicks += 2;
+    }
+
+
+
 }
 
 firstUpdate.onclick = function() {
     if (numOfClicks >= 30) {
-        document.body.style.backgroundImage = "url('img/background.jpg')";
+        firstStage = true;
+        numOfClicks = numOfClicks - 30;
+        document.body.style.backgroundImage = "url('img/background.png')";
+        firstUpdate.style.visibility = "hidden";
     }}
+
+secondUpdate.onclick = function() {
+    if (numOfClicks >= 50) {
+        secondStage = true;
+        numOfClicks = numOfClicks - 50;
+        document.body.style.backgroundImage = "url('img/ghostbg.png')";
+        secondUpdate.style.visibility = "hidden";
+    }
+}
